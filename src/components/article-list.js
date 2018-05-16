136f406c4481de0f20e101336e70955e4725316d
Your Name <you@example.com>
2018-05-16 17:32:05 +0300

    HT1.3

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Article from './article'
 import accordion from '../decorators/accordion'
 
 function ArticleList(props) {
     const articleElements = props.articles.map(article => <li key={article.id}>
         <Article article={article}
-                 isOpen={article.id === props.openItemId}
-                 toggleOpen={props.toggleOpenItem}
+            isOpen={article.id === props.openItemId}
+            toggleOpen={props.toggleOpenItem}
         />
     </li>)
 
--- src/components/article.js
+++ src/components/article.js
@@ -1,17 +1,19 @@
 import React, { PureComponent } from 'react'
+import CommentList from './comment-list'
 
 class Article extends PureComponent {
     render() {
         const { article, isOpen } = this.props
         return (
             <div>
-                <h2 ref = {this.setTitleRef}>
+                <h2 ref={this.setTitleRef}>
                     {article.title}
                 </h2>
-                <button onClick = {this.toggleOpen}>
+                <button onClick={this.toggleOpen}>
                     {isOpen ? 'close' : 'open'}
                 </button>
                 {this.getBody()}
+                {this.getComments()}
             </div>
         )
     }
@@ -28,9 +30,19 @@ class Article extends PureComponent {
         )
     }
 
+    getComments() {
+        const { isOpen, article } = this.props
+        let comments = article.comments ? article.comments : [];
+
+        if (isOpen)
+            return <CommentList comments={comments} />
+        else
+            return <div>All comments: {comments.length}</div>;
+    }
+
     setTitleRef = title => console.log('---', title)
 
     toggleOpen = () => this.props.toggleOpen(this.props.article.id)
 }
 
-export default Article
+export default Article;
