ba4ba99de5d96809dad8ec276457650bf896f906
SmotrovaLilit <lilit2990@gmail.com>
2018-05-20 02:04:03 +0300

    HT2.1 Написать тесты на список комментов

--- src/components/comments/comment-list.js
+++ src/components/comments/comment-list.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Comment from './comment'
-import toggleOpen from '../decorators/toggleOpen'
+import toggleOpen from '../../decorators/toggleOpen'
 
-class CommentList extends Component {
+export class CommentList extends Component {
   static propTypes = {
     comments: PropTypes.array,
     //from toggleOpen decorator
@@ -16,7 +16,9 @@ class CommentList extends Component {
     const text = isOpen ? 'hide comments' : 'show comments'
     return (
       <div>
-        <button onClick={toggleOpen}>{text}</button>
+        <button className="test__comment-button" onClick={toggleOpen}>
+          {text}
+        </button>
         {this.getBody()}
       </div>
     )
@@ -26,17 +28,18 @@ class CommentList extends Component {
     const { comments, isOpen } = this.props
     if (!isOpen) return null
 
-    const body = comments.length ? (
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>
-            <Comment comment={comment} />
-          </li>
-        ))}
-      </ul>
-    ) : (
-      <h3>No comments yet</h3>
-    )
+    const body =
+      comments && comments.length ? (
+        <ul>
+          {comments.map((comment) => (
+            <li className="test__comment-list_item" key={comment.id}>
+              <Comment comment={comment} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h3>No comments yet</h3>
+      )
 
     return <div>{body}</div>
   }
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,11 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import jsdom from 'jsdom'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+//пришлось добавить код ниже, у меня почему то не работал mount в тестах, не смогла разобраться почему у вас работало на уроке
+//https://github.com/airbnb/enzyme/issues/341
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.document = doc
+global.window = doc.defaultView
