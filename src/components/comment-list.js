dabce30cc631d7cb274e8d45e7daca326ed84a1b
KatePavlovich <asylumhere@mail.ru>
2018-05-20 14:04:32 +0300

    homeTask#2

--- src/components/comment-list.js
+++ src/components/comment-list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Comment from './comment'
 import toggleOpen from '../decorators/toggleOpen'
+import CSSTransition from 'react-addons-css-transition-group'
 
 class CommentList extends Component {
   static propTypes = {
@@ -16,8 +17,16 @@ class CommentList extends Component {
     const text = isOpen ? 'hide comments' : 'show comments'
     return (
       <div>
-        <button onClick={toggleOpen}>{text}</button>
-        {this.getBody()}
+        <button onClick={toggleOpen} className="test__button">{text}</button>
+        <CSSTransition
+          transitionAppear
+          transitionName="article"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}
+          transitionAppearTimeout={1000}
+        >
+          {this.getBody()}
+        </CSSTransition>
       </div>
     )
   }
@@ -29,7 +38,7 @@ class CommentList extends Component {
     const body = comments.length ? (
       <ul>
         {comments.map((comment) => (
-          <li key={comment.id}>
+          <li key={comment.id} className="test__comment-list_item">
             <Comment comment={comment} />
           </li>
         ))}
