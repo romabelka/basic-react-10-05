f34fbe6fa3135325a52f1e8cfdd8de2c627b59a9
Marina Golumberskaya <8sombrero_galaxy8@mail.ru>
2018-05-20 20:46:21 +0630

    add propTypes

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import Article from './article'
+import PropTypes from 'prop-types'
 import accordion from '../decorators/accordion'
 
 export class ArticleList extends Component {
+  static propTypes = {
+    articles: PropTypes.array.isRequired
+  }
+
   componentWillMount() {
     this.props.fetchData && this.props.fetchData()
   }
