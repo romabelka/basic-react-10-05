9637ff4fee60dde16a4a348774a71443cf1a66d3
Roman Iakobchuk <romania@wix.com>
2018-05-21 22:29:51 +0300

    add simple redux flow

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import { findDOMNode } from 'react-dom'
 import ArticleList from './components/article-list'
 import Chart from './components/chart'
 import UserForm from './components/user-form'
-import Filters from './components/filters/index'
+import Filters from './components/filters'
+import Counter from './components/counter'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div>
         <UserForm />
+        <Counter />
         <Filters articles={articles} />
         <ArticleList articles={articles} ref={this.setArticleListRef} />
         <Chart articles={articles} />
