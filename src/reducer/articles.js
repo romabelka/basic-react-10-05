c5dfc5bd9c320fbc67c9465da5983f30d9c53472
lilit <lilit2990@gmail.com>
2018-05-30 20:45:37 +0300

    HT4.2 Добавить в CommentList форму нового коммента HT4.3 Написать middleware для генерации случайных id HT4.4 Реализовать добавление комментария к статье

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,4 +1,4 @@
-import { DELETE_ARTICLE } from '../constants'
+import { ADD_COMMENT, DELETE_ARTICLE } from '../constants'
 import { normalizedArticles } from '../fixtures'
 
 const defaultArticles = normalizedArticles.reduce(
@@ -14,6 +14,17 @@ export default (articleState = defaultArticles, action) => {
   const { type, payload } = action
 
   switch (type) {
+    case ADD_COMMENT:
+      const article = articleState[payload.articleId]
+
+      return {
+        ...articleState,
+        [article.id]: {
+          ...article,
+          comments: [...article.comments, payload.id]
+        }
+      }
+
     case DELETE_ARTICLE:
       return articleState.filter((article) => article.id !== payload.id)
 
