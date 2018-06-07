977691ab5cbf76d54a75bdaa57fd699ddaf9d55c
Roman Iakobchuk <romania@wix.com>
2018-06-07 22:55:41 +0300

    add browser router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
